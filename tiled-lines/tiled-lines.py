1d4aed8c0ce4f46e4b52db69ec0c63e9fcb51f9b
Ana Ulin <ana@ulin.org>
2018-12-27 19:41:45 -0800

    Add experiment with more colors, and a dark background.

--- tiled-lines/tiled-lines.py
+++ tiled-lines/tiled-lines.py
@@ -6,15 +6,16 @@ IMG_WIDTH = 3840
 
 LINE_WIDTH = 5
 
-PALETTE = {
-    'background': '#090909',
-    'colors': ['#4B5043', '#9BC4BC', '#D3FFE9', '#8DDBE0']
-}
+PALETTE = ['#4B5043', '#9BC4BC', '#D3FFE9', '#8DDBE0']
+
+PALETTE_2 = ['#EEC525', '#C05F73', '#9D2045', '#511253',
+             '#3A1B3D', '#B892FF', '#FFC2E2', '#FF90B3', '#EF7A85', '#6E44FF']
 
 
 def hex_to_tuple(hex):
     hex = hex.lstrip('#')
     return tuple(int(hex[i:i+2], 16)/255 for i in (0, 2, 4))
+
 
 def draw_line(ctx, x, y, width, height, line_width):
     leftToRight = random.random() >= 0.5
@@ -33,19 +34,21 @@ def draw_line(ctx, x, y, width, height, line_width):
     ctx.set_line_cap(cairo.LineCap.ROUND)
 
     g = cairo.LinearGradient(start[0], start[1], end[0], end[1])
-    colors = [hex_to_tuple(c) for c in random.choices(PALETTE['colors'], k=2)]
+    colors = [hex_to_tuple(c) for c in random.choices(PALETTE_2, k=2)]
     g.add_color_stop_rgb(0, *colors[0])
     g.add_color_stop_rgb(1, *colors[1])
     ctx.set_source(g)
     #ctx.set_source_rgb(0, 0, 0)
     ctx.stroke()
 
+
 def main(filename="output.png", step=50, line_width=5):
     ims = cairo.ImageSurface(cairo.FORMAT_ARGB32, IMG_WIDTH, IMG_HEIGHT)
     ctx = cairo.Context(ims)
 
-    # Make background white
-    ctx.set_source_rgb(256, 256, 256)
+    # Make background solid color
+    #ctx.set_source_rgb(256, 256, 256)
+    ctx.set_source_rgb(0, 0, 0)
     ctx.rectangle(0, 0, IMG_WIDTH, IMG_HEIGHT)
     ctx.fill()
 
@@ -57,5 +60,5 @@ def main(filename="output.png", step=50, line_width=5):
 
 
 if __name__ == "__main__":
-    for i in [1, 2, 3]:
-        main(filename="output-{}-color.png".format(i), step=i*30, line_width=20)
+    for i in [40, 100]:
+        main(filename="output-{}-dark-bg.png".format(i), step=i, line_width=20)
